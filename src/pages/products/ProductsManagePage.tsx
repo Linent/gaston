fc9512a6ed779af1ca4ceed4a77d7d114bc71823
Anderson <andersoneduardoop@ufps.edu.co>
2024-01-26 23:06:55 -0500

    acciones

--- src/pages/products/ProductsManagePage.tsx
+++ src/pages/products/ProductsManagePage.tsx
@@ -10,8 +10,10 @@ import {
   useDisclosure,
   Divider,
   Button,
+  Tooltip,
 } from "@nextui-org/react";
-import { CreateProductModal } from "./components";
+
+import { CreateProductModal,EditIcon,DeleteIcon, EyeIcon } from "./components";
 import { IProduct } from "../../interfaces";
 
 const columns = [
@@ -32,6 +34,10 @@ const columns = [
     key: "precio",
     label: "Precio",
   },
+  {
+    key: "ACTIONS", 
+    label: "actions"
+  },
 ];
 
 const Producto: React.FC<IProduct> = ({
@@ -45,6 +51,23 @@ const Producto: React.FC<IProduct> = ({
     <TableCell>{descripcion}</TableCell>
     <TableCell>{precio}</TableCell>
     <TableCell>{existencias}</TableCell>
+    <TableCell className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+    </TableCell>
   </TableRow>
 );
 const data: IProduct[] = [
